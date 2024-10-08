358c10e67f19431cefccefd61ce2b9156e6532ed
rustedchip <rustedchip@gmail.com>
2024-10-08 18:57:37 +0000

    entrypoint :: messages-update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Environment variables
 OWNCLOUD_URL=${OWNCLOUD_URL}
 OWNCLOUD_USERNAME=${OWNCLOUD_USERNAME}
 OWNCLOUD_PASSWORD=${OWNCLOUD_PASSWORD}
@@ -9,48 +7,41 @@ REMOTE_SYNC_DIR=${REMOTE_SYNC_DIR}
 EXCLUDE_PATTERNS=${OWNCLOUD_EXCLUDE}
 SYNC_INTERVAL=${SYNC_INTERVAL}
 
-# Path to the exclude list
+# this-version-of-owncloud-needs-exclude-file
+
 EXCLUDE_FILE="/tmp/exclude-list.txt"
 
-# Function to create the exclude file
 create_exclude_file() {
     echo "Creating exclude list file at $EXCLUDE_FILE..."
     if [ -n "$EXCLUDE_PATTERNS" ]; then
         echo "$EXCLUDE_PATTERNS" | tr ',' '\n' > "$EXCLUDE_FILE"
-        echo "Exclude file created with the following patterns:"
+        echo "exclude-file-created :: patterns :: "
         cat "$EXCLUDE_FILE"
     else
-        # Create an empty exclude list file
         touch "$EXCLUDE_FILE"
-        echo "Exclude file created but is empty (no patterns to exclude)."
+        echo "exclude-file-created :: no-patterns"
     fi
 }
 
-# Function to perform one-time sync with ownCloud
 sync_files() {
-    echo "Running ownCloud sync..."
-
-    # Check if exclude file exists and use it
+    echo "running-owncloud-sync"
     if [ -f "$EXCLUDE_FILE" ]; then
-        echo "Using exclude file: $EXCLUDE_FILE"
+        echo "using-exclude-file :: $EXCLUDE_FILE"
         owncloudcmd --user "$OWNCLOUD_USERNAME" --password "$OWNCLOUD_PASSWORD" --exclude "$EXCLUDE_FILE" "$LOCAL_SYNC_DIR" "$OWNCLOUD_URL$REMOTE_SYNC_DIR"
     else
-        echo "Exclude file not found. Skipping --exclude option."
+        echo "exclude-file-not-found :: skipping-exclude-option"
         owncloudcmd --user "$OWNCLOUD_USERNAME" --password "$OWNCLOUD_PASSWORD" "$LOCAL_SYNC_DIR" "$OWNCLOUD_URL$REMOTE_SYNC_DIR"
     fi
 
-    echo "Sync complete."
+    echo "[ sync-has-been-completed ]"
 }
 
-# Ensure the sync directory exists
 mkdir -p "$LOCAL_SYNC_DIR"
 
-# Create exclude file
 create_exclude_file
 
-# Run the sync every X seconds
 while true; do
     sync_files
-    echo "Waiting $SYNC_INTERVAL seconds before next sync..."
+    echo "waiting-$SYNC_INTERVAL-seconds-before-next-sync"
     sleep "$SYNC_INTERVAL"
 done
